821f8042b7263711e2422c2fe697981a7806911c
Alisa Valiullina <avaliull@f1r2s5.codam.nl>
2026-07-31 20:15:19 +0200

    shrubberies

--- ex02/src/ShrubberyCreationForm.cpp
+++ ex02/src/ShrubberyCreationForm.cpp
@@ -6,12 +6,13 @@
 /*   By: avaliull <avaliull@student.codam.nl>              +#+                */
 /*                                                        +#+                 */
 /*   Created: 2026/07/31 18:52:11 by avaliull            #+#    #+#           */
-/*   Updated: 2026/07/31 20:00:10 by avaliull            ########   odam.nl   */
+/*   Updated: 2026/07/31 20:14:17 by avaliull            ########   odam.nl   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ShrubberyCreationForm.hpp"
 #include <iostream>
+#include <fstream>
 
 /*	Canonical form stuff	*/
 ShrubberyCreationForm::ShrubberyCreationForm(const std::string& target)
@@ -29,9 +30,23 @@ ShrubberyCreationForm::~ShrubberyCreationForm() {}
 /**/
 
 void	ShrubberyCreationForm::execute(const Bureaucrat& bureaucrat) const {
-	//try {
-		this->confirmCanExecute(bureaucrat);
-//	}
-//	catch (const FormException& e) {
-//	}
+	this->confirmCanExecute(bureaucrat);
+	std::ofstream	plantation(this->getName());
+	unsigned int	tree_count = this->getExecuteGrade() - bureaucrat.getGrade();
+	do {
+		plantation << "          &&& &&  & &&\n\
+      && &\\/&\\|& ()|/ @, &&\n\
+      &\\/(/&/&||/& /_/)_&/_&\n\
+   &() &\\/&|()|/&\\/ \'%\" & ()\n\
+  &_\\_&&_\\ |& |&&/&__%_/_& &&\n\
+&&   && & &| &| /& & % ()& /&&\n\
+ ()&_---()&\\&\\|&&-&&--%---()~\n\
+     &&     \\|||\n\
+             |||\n\
+             |||\n\
+             |||\n\
+       , -=-~  .-^- _\n\
+ejm97         `";
+		tree_count--;
+	} while(tree_count);
 }
